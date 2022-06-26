0a40d166fb7dfd70d059b9268bf20c06519ac01d
Edgar Aquino <eeaquino@hotmail.com>
2022-06-25 20:46:14 -0700

    Added Interface

---
+++ ActionTimerExecutor/IActionTimer.cs
@@ -0,0 +1,11 @@
+﻿namespace ActionTimerExecutor
+{
+    public interface IActionTimer
+    {
+        bool AddAction(string action, Func<Task> actionToExecute, int intervalMilliseconds, bool noWait);
+        void Dispose();
+        List<string> GetErrors(string action, bool noWait);
+        bool RemoveAction(string action, bool noWait);
+        void Start(int milliseconds = 1000, CancellationToken? ct = null);
+    }
+}
